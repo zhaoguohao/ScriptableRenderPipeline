a60bb85320f8736e1240f4ec8270fa6b5c23bdb0
Nerites <paula.s.ning@gmail.com>
2018-11-01 13:13:46 -0700

    Added a kernel to DepthPyramid.compute for single-pass instanced compatability

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/RenderPass/MipGenerator.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/RenderPass/MipGenerator.cs
@@ -12,6 +12,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         ComputeShader m_ColorPyramidCS;
 
         int m_DepthDownsampleKernel;
+        int m_DepthDownsampleSPIKernel;
         int m_ColorDownsampleKernel;
         int m_ColorDownsampleKernelCopyMip0;
         int m_ColorGaussianKernel;
@@ -25,6 +26,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             m_ColorPyramidCS = asset.renderPipelineResources.shaders.colorPyramidCS;
 
             m_DepthDownsampleKernel = m_DepthPyramidCS.FindKernel("KDepthDownsample8DualUav");
+            m_DepthDownsampleSPIKernel = m_DepthPyramidCS.FindKernel("KDepthDownsample8DualUavSPI");
             m_ColorDownsampleKernel = m_ColorPyramidCS.FindKernel("KColorDownsample");
             m_ColorDownsampleKernelCopyMip0 = m_ColorPyramidCS.FindKernel("KColorDownsampleCopyMip0");
             m_ColorGaussianKernel = m_ColorPyramidCS.FindKernel("KColorGaussian");
@@ -45,7 +47,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             HDUtils.CheckRTCreated(texture);
 
             var cs     = m_DepthPyramidCS;
-            int kernel = m_DepthDownsampleKernel;
+            int kernel = (XRGraphics.stereoRenderingMode == XRGraphics.StereoRenderingMode.SinglePassInstanced) ? m_DepthDownsampleSPIKernel : m_DepthDownsampleKernel;
 
             // TODO: Do it 1x MIP at a time for now. In the future, do 4x MIPs per pass, or even use a single pass.
             // Note: Gather() doesn't take a LOD parameter and we cannot bind an SRV of a MIP level,
@@ -72,7 +74,11 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                 cmd.SetComputeIntParams(   cs,         HDShaderIDs._DstOffset,         m_DstOffset);
                 cmd.SetComputeTextureParam(cs, kernel, HDShaderIDs._DepthMipChain,     texture);
 
-                cmd.DispatchCompute(cs, kernel, HDUtils.DivRoundUp(dstSize.x, 8), HDUtils.DivRoundUp(dstSize.y, 8), 1);
+                for (int eye = 0; eye < XRGraphics.NumSlices; eye++)
+                {
+                    cmd.SetGlobalInt(Shader.PropertyToID("_ComputeEyeIndex"), (int)eye);
+                    cmd.DispatchCompute(cs, kernel, HDUtils.DivRoundUp(dstSize.x, 8), HDUtils.DivRoundUp(dstSize.y, 8), 1);
+                }
             }
         }
 
