6c1751b0ce9ee28a0d372fed4fa46710e47448cc
Stephane Laroche <stephanel@unity3d.com>
2018-11-14 16:58:23 -0500

    Make new StackLit shader graph asset also default to the "Shader Graphs" path in the Shader menu.

--- com.unity.render-pipelines.high-definition/Editor/ShaderGraph/AssetCallbacks/CreateStackLitShaderGraph.cs
+++ com.unity.render-pipelines.high-definition/Editor/ShaderGraph/AssetCallbacks/CreateStackLitShaderGraph.cs
@@ -16,6 +16,7 @@ namespace UnityEditor.ShaderGraph
         {
             var graph = new MaterialGraph();
             graph.AddNode(new StackLitMasterNode());
+            graph.path = "Shader Graphs";
             File.WriteAllText(pathName, EditorJsonUtility.ToJson(graph));
             AssetDatabase.Refresh();
         }
