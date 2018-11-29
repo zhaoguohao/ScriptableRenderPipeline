c1f98eb0166d7f933715f953a4944bb24d2c8990
spaztronaut <wsndrs@gmail.com>
2018-11-29 11:25:57 -0800

    math_basic hlsl file and new multiply node

---
+++ com.unity.shadergraph/Editor/Data/Nodes/Math/Basic/Math_Basic.hlsl
@@ -0,0 +1,29 @@
+#ifndef UNITY_SHADERGRAPH_MATH_BASIC
+#define UNITY_SHADERGRAPH_MATH_BASIC
+
+/****************************************************************
+    
+    Multiply
+
+****************************************************************/
+real Unity_Multiply(real a, real b)
+{
+    return a * b;
+}
+
+real2 Unity_Multiply(real2 a, real2 b)
+{
+    return a * b;
+}
+
+real3 Unity_Multiply(real3 a, real3 b)
+{
+    return a * b;
+}
+
+real4 Unity_Multiply(real4 a, real4 b)
+{
+    return a * b;
+}
+
+#endif // UNITY_SHADERGRAPH_MATH_BASIC
---
+++ com.unity.shadergraph/Editor/Data/Nodes/Math/Basic/NewMultiplyNode.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEditor.ShaderGraph
+{
+    class NewMultiplyNode : IShaderNodeType
+    {
+        InputPortRef m_aPort;
+        InputPortRef m_bPort;
+        OutputPortRef m_OutPort;
+
+        public void Setup(ref NodeSetupContext context)
+        {
+            m_aPort = context.CreateInputPort(0, "A", PortValue.Vector1(0.5f));
+            m_aPort.bindingType = new BindingUV(0);
+            m_bPort = context.CreateInputPort(1, "B", PortValue.Vector1(0.5f));
+            m_OutPort = context.CreateOutputPort(2, "Out", PortValueType.Vector1);
+
+            var type = new NodeTypeDescriptor
+            {
+                path = "Math/Basic",
+                name = "New Multiply",
+                inputs = new List<InputPortRef> { m_aPort, m_bPort },
+                outputs = new List<OutputPortRef> { m_OutPort }
+            };
+
+            context.CreateType(type);
+        }
+
+        HlslSourceRef m_Source;
+
+        public void OnChange(ref NodeTypeChangeContext context)
+        {
+            // TODO: Figure out what should cause the user to create the hlsl source
+            // TODO: How does sharing files between multiple node types work?
+            if (!m_Source.isValid)
+            {
+                m_Source = context.CreateHlslSource("Packages/com.unity.shadergraph/Editor/Data/Nodes/Math/Basic/Math_Basic.hlsl");
+            }
+
+            foreach (var node in context.addedNodes)
+            {
+                context.SetHlslFunction(node, new HlslFunctionDescriptor
+                {
+                    source = m_Source,
+                    name = "Unity_Multiply",
+                    arguments = new HlslArgumentList { m_aPort, m_bPort },
+                    returnValue = m_OutPort
+                });
+            }
+        }
+    }
+}
