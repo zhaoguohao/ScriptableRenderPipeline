c3599c88e457d3fbadd0722593013d9b900d599f
ChrisTchou <ctchou@unity3d.com>
2018-11-29 11:30:05 -0800

    NEW! checkerboard node

---
+++ com.unity.shadergraph/Editor/Data/Nodes/Procedural/CheckerboardNode.hlsl
@@ -0,0 +1,17 @@
+﻿real3 Unity_Checkerboard(
+    real2 UV,
+    real3 ColorA,
+    real3 ColorB,
+    real2 Frequency)
+{
+    UV = (UV.xy + 0.5) * Frequency;
+    real4 derivatives = real4(ddx(UV), ddy(UV));
+    real2 duv_length = sqrt(real2(dot(derivatives.xz, derivatives.xz), dot(derivatives.yw, derivatives.yw)));
+    real width = 1.0;
+    real2 distance3 = 4.0 * abs(frac(UV + 0.25) - 0.5) - width;
+    real2 scale = 0.35 / duv_length.xy;
+    real freqLimiter = sqrt(clamp(1.1f - max(duv_length.x, duv_length.y), 0.0, 1.0));
+    real2 vector_alpha = clamp(distance3 * scale.xy, -1.0, 1.0);
+    real alpha = saturate(0.5f + 0.5f * vector_alpha.x * vector_alpha.y * freqLimiter);
+    return lerp(ColorA, ColorB, alpha.xxx);
+}
---
+++ com.unity.shadergraph/Editor/Data/Nodes/Procedural/CheckerboardNode.new.cs
@@ -0,0 +1,67 @@
+﻿using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEditor.ShaderGraph
+{
+    class NewCheckerboardNode : IShaderNodeType
+    {
+        InputPortRef m_inPortUV;
+        InputPortRef m_inPortColorA;
+        InputPortRef m_inPortColorB;
+        InputPortRef m_inPortFrequency;
+        OutputPortRef m_outPortResult;
+
+        public void Setup(ref NodeSetupContext context)
+        {
+            m_inPortUV = context.CreateInputPort(0, "UV", PortValue.Vector2(Vector2.zero));
+            m_inPortColorA = context.CreateInputPort(1, "ColorA", PortValue.Vector3(Vector3.zero));
+            m_inPortColorB = context.CreateInputPort(2, "ColorB", PortValue.Vector3(Vector3.one));
+            m_inPortFrequency = context.CreateInputPort(3, "Frequency", PortValue.Vector2(Vector2.one));
+            m_outPortResult = context.CreateOutputPort(4, "Out", PortValueType.Vector3);
+            var type = new NodeTypeDescriptor
+            {
+                path = "Procedural/Checkerboard",
+                name = "New Checkerboard",
+                inputs = new List<InputPortRef> { m_inPortUV, m_inPortColorA, m_inPortColorB, m_inPortFrequency },
+                outputs = new List<OutputPortRef> { m_outPortResult }
+            };
+            context.CreateType(type);
+        }
+
+        HlslSourceRef m_Source;
+        public void OnChange(ref NodeTypeChangeContext context)
+        {
+            if (!m_Source.isValid)
+            {
+                m_Source = context.CreateHlslSource("Packages/com.unity.shadergraph/Editor/Data/Nodes/Procedural/CheckerboardNode.hlsl");
+            }
+
+            // process newly created nodes
+            foreach (NodeRef node in context.addedNodes)
+            {
+                context.SetHlslFunction(
+                    node,
+                    new HlslFunctionDescriptor
+                    {
+                        source = m_Source,
+                        name = "Unity_Checkerboard",
+                        arguments = new HlslArgumentList
+                        {
+                            m_inPortUV,
+                            m_inPortColorA,
+                            m_inPortColorB,
+                            m_inPortFrequency
+                        },
+                        returnValue = m_outPortResult
+                    }
+                );
+            }
+
+            foreach (var node in context.modifiedNodes)
+            {
+                // .... no controls
+            }
+        }
+    }
+}
