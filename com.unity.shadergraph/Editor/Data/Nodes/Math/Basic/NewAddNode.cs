0414be1e9391ba593235ba0e5cc6337b76becfb5
martint-unity <martint@unity3d.com>
2018-11-29 21:47:21 +0100

    Added rest of basic math nodes

--- com.unity.shadergraph/Editor/Data/Nodes/Math/Basic/NewAddNode.cs
+++ com.unity.shadergraph/Editor/Data/Nodes/Math/Basic/NewAddNode.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace UnityEditor.ShaderGraph
 {
-    class NewDivideNode : IShaderNodeType
+    class NewAddNode : IShaderNodeType
     {
         InputPortRef m_APort;
         InputPortRef m_BPort;
@@ -12,14 +12,14 @@ namespace UnityEditor.ShaderGraph
 
         public void Setup(ref NodeSetupContext context)
         {
-            m_APort = context.CreateInputPort(0, "A", PortValue.DynamicVector(0.5f));
-            m_BPort = context.CreateInputPort(1, "B", PortValue.DynamicVector(0.5f));
-            m_OutPort = context.CreateOutputPort(2, "Out", PortValueType.DynamicVector);
+            m_APort = context.CreateInputPort(0, "A", PortValue.DynamicVector(0f));
+            m_BPort = context.CreateInputPort(1, "B", PortValue.DynamicVector(0f));
+            m_OutPort = context.CreateOutputPort(2, "Out", PortValueType.Vector1);
 
             var type = new NodeTypeDescriptor
             {
                 path = "Math/Basic",
-                name = "New Divide",
+                name = "New Add",
                 inputs = new List<InputPortRef> { m_APort, m_BPort },
                 outputs = new List<OutputPortRef> { m_OutPort }
             };
@@ -38,7 +38,7 @@ namespace UnityEditor.ShaderGraph
                 context.SetHlslFunction(node, new HlslFunctionDescriptor
                 {
                     source = m_Source,
-                    name = "Unity_Divide",
+                    name = "Unity_Add",
                     arguments = new HlslArgumentList { m_APort, m_BPort },
                     returnValue = m_OutPort
                 });
