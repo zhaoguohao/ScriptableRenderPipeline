811f5a57dcafb4f4f06832275ec3ed222bc4a1d2
Nerites <paula.s.ning@gmail.com>
2018-12-10 09:44:20 -0800

    Reverted SPI changes to deferred-specific shaders and code

--- com.unity.render-pipelines.high-definition/Runtime/Material/GBufferManager.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Material/GBufferManager.cs
@@ -48,7 +48,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
 
             for (int gbufferIndex = 0; gbufferIndex < m_BufferCount; ++gbufferIndex)
             {
-                m_RTs[gbufferIndex] = RTHandles.Alloc(Vector2.one, colorFormat: rtFormat[gbufferIndex], sRGB: sRGBFlags[gbufferIndex], filterMode: FilterMode.Point, name: string.Format("GBuffer{0}", gbufferIndex), enableRandomWrite: enableWrite[gbufferIndex]); 
+                m_RTs[gbufferIndex] = RTHandles.Alloc(Vector2.one, colorFormat: rtFormat[gbufferIndex], sRGB: sRGBFlags[gbufferIndex], filterMode: FilterMode.Point, name: string.Format("GBuffer{0}", gbufferIndex), enableRandomWrite: enableWrite[gbufferIndex], requireStereoSupport: false); // Deferred unsupported in VR
                 m_RTIDs[gbufferIndex] = m_RTs[gbufferIndex].nameID;
                 m_TextureShaderIDs[gbufferIndex] = HDShaderIDs._GBufferTexture[gbufferIndex];
 
