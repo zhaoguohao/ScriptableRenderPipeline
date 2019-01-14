63152a8d86992f40010fb4b7e86ff32e3e9624a0
martint-unity <martint@unity3d.com>
2019-01-14 18:15:11 +0100

    Added functionality to create graph item

--- com.unity.shadergraph/Editor/AssetCallbacks/CreatePBRShaderGraph.cs
+++ com.unity.shadergraph/Editor/AssetCallbacks/CreatePBRShaderGraph.cs
@@ -1,24 +1,15 @@
 using System.IO;
 using UnityEditor.ProjectWindowCallback;
+using UnityEngine;
 
 namespace UnityEditor.ShaderGraph
 {
-    class CreatePBRShaderGraph : EndNameEditAction
+    class CreatePBRShaderGraph
     {
         [MenuItem("Assets/Create/Shader/PBR Graph", false, 208)]
         public static void CreateMaterialGraph()
         {
-            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, CreateInstance<CreatePBRShaderGraph>(),
-                string.Format("New Shader Graph.{0}", ShaderGraphImporter.Extension), null, null);
-        }
-
-        public override void Action(int instanceId, string pathName, string resourceFile)
-        {
-            var graph = new MaterialGraph();
-            graph.AddNode(new PBRMasterNode());
-            graph.path = "Shader Graphs";
-            File.WriteAllText(pathName, EditorJsonUtility.ToJson(graph));
-            AssetDatabase.Refresh();
+            GraphUtil.CreateNewGraph(new PBRMasterNode());
         }
     }
 }
--- com.unity.shadergraph/Editor/AssetCallbacks/CreateUnlitShaderGraph.cs
+++ com.unity.shadergraph/Editor/AssetCallbacks/CreateUnlitShaderGraph.cs
@@ -1,24 +1,15 @@
 using System.IO;
 using UnityEditor.ProjectWindowCallback;
+using UnityEngine;
 
 namespace UnityEditor.ShaderGraph
 {
-    class CreateUnlitShaderGraph : EndNameEditAction
+    class CreateUnlitShaderGraph
     {
         [MenuItem("Assets/Create/Shader/Unlit Graph", false, 208)]
         public static void CreateMaterialGraph()
         {
-            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, CreateInstance<CreateUnlitShaderGraph>(),
-                string.Format("New Shader Graph.{0}", ShaderGraphImporter.Extension), null, null);
-        }
-
-        public override void Action(int instanceId, string pathName, string resourceFile)
-        {
-            var graph = new MaterialGraph();
-            graph.AddNode(new UnlitMasterNode());
-            graph.path = "Shader Graphs";
-            File.WriteAllText(pathName, EditorJsonUtility.ToJson(graph));
-            AssetDatabase.Refresh();
+            GraphUtil.CreateNewGraph(new UnlitMasterNode());
         }
     }
 }
