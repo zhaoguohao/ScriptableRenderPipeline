a2f626beace2ce0f85c8742ff88c211d4b6ff1ce
Alexandra Lindman <alexandral@hq.unity3d.com>
2019-01-25 08:56:21 -0800

    first run organizing blackboard

---
+++ com.unity.shadergraph/Editor/Drawing/Blackboard/BlackboardFieldPortView.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Linq;
+using System.Globalization;
+using UnityEditor.Graphing;
+using UnityEditor.Graphing.Util;
+using UnityEngine;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+using Toggle = UnityEngine.UIElements.Toggle;
+using UnityEditor.Experimental.GraphView;
+
+namespace UnityEditor.ShaderGraph.Drawing
+{
+    class BlackboardFieldPortView : VisualElement
+    {
+        readonly BlackboardField m_BlackboardField;
+        readonly AbstractMaterialGraph m_Graph;
+
+        InputDescriptor m_Port;
+
+        static Type s_ContextualMenuManipulator = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypesOrNothing()).FirstOrDefault(t => t.FullName == "UnityEngine.UIElements.ContextualMenuManipulator");
+        
+        public BlackboardFieldPortView(BlackboardField blackboardField, AbstractMaterialGraph graph, InputDescriptor port)
+        {
+            styleSheets.Add(Resources.Load<StyleSheet>("Styles/ShaderGraphBlackboard"));
+            m_BlackboardField = blackboardField;
+            m_Graph = graph;
+            m_Port = port;
+
+            AddRow("Default", new TextField());
+            //put the drawing of port binding options here based on slot type
+
+//            AddRow("Type", new TextField());
+//            AddRow("Exposed", new Toggle(null));
+//            AddRow("Range", new Toggle(null));
+//            AddRow("Default", new TextField());
+//            AddRow("Tooltip", new TextField());
+
+
+            AddToClassList("sgblackboardFieldPortView");
+
+            
+        }
+
+        VisualElement CreateRow(string labelText, VisualElement control)
+        {
+            VisualElement rowView = new VisualElement();
+
+            rowView.AddToClassList("rowView");
+
+            Label label = new Label(labelText);
+
+            label.AddToClassList("rowViewLabel");
+            rowView.Add(label);
+
+            control.AddToClassList("rowViewControl");
+            rowView.Add(control);
+
+            return rowView;
+        }
+
+        VisualElement AddRow(string labelText, VisualElement control)
+        {
+            VisualElement rowView = CreateRow(labelText, control);
+            Add(rowView);
+            return rowView;
+        }
+
+        void RemoveElements(VisualElement[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (elements[i].parent == this)
+                    Remove(elements[i]);
+            }
+        }
+
+        void DirtyNodes(ModificationScope modificationScope = ModificationScope.Node)
+        {
+            foreach (var node in m_Graph.GetNodes<PropertyNode>())
+                node.Dirty(modificationScope);
+        }
+    }
+}
