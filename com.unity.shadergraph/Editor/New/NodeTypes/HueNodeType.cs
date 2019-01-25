e048b1f9d70730c02d35cdda64cd26a938ccf0eb
Matt Dean <matthewdean3d@gmail.com>
2019-01-25 14:38:29 -0800

    Fix merge issues

---
+++ com.unity.shadergraph/Editor/New/NodeTypes/HueNodeType.cs
@@ -0,0 +1,34 @@
+namespace UnityEditor.ShaderGraph
+{
+    sealed class HueNodeType : ShaderNodeType
+    {
+        InputDescriptor m_InPort = new InputDescriptor(0, "In", ConcreteSlotValueType.Vector3, new ColorControl());
+        InputDescriptor m_OffsetPort = new InputDescriptor(1, "Offset", ConcreteSlotValueType.Vector1, new Vector1Control());
+        InputDescriptor m_ModeParameter = new InputDescriptor(3, "Mode", ConcreteSlotValueType.Vector1, new PopupControl(new string[] { "Degrees", "Normalized" }, 0));
+        OutputDescriptor m_OutPort = new OutputDescriptor(2, "Out", ConcreteSlotValueType.Vector3);
+
+        internal override void Setup(ref NodeSetupContext context)
+        {
+            context.CreateNodeType(new NodeTypeDescriptor
+            {
+                path = "INTERNAL",
+                name = "Hue",
+                inPorts = new InputDescriptor[] { m_InPort, m_OffsetPort },
+                outPorts = new OutputDescriptor[] { m_OutPort },
+                parameters = new InputDescriptor[] { m_ModeParameter },
+                preview = true
+            });
+        }
+
+        internal override void OnNodeAdded(NodeChangeContext context, NodeRef node)
+        {
+            context.SetHlslFunction(node, new HlslFunctionDescriptor
+            {
+                name = "Unity_Hue",
+                source = HlslSource.File("Packages/com.unity.shadergraph/Editor/New/NodeIncludes/ArtisticNodes.hlsl"),
+                inArguments = new InputDescriptor[] { m_InPort, m_OffsetPort, m_ModeParameter },
+                outArguments = new OutputDescriptor[] { m_OutPort }
+            });
+        }
+    }
+}
