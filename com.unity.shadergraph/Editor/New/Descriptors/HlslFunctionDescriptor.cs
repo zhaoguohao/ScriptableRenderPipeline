a2283b37c0e6481e554b89cfa7dfc4f8f9bcef61
Matt Dean <matthewdean3d@gmail.com>
2019-01-26 12:41:57 -0800

    Add function generation from descriptors and view

---
+++ com.unity.shadergraph/Editor/New/Descriptors/HlslFunctionDescriptor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityEditor.ShaderGraph
+{
+    [Serializable]
+    internal class HlslFunctionDescriptor
+    {
+        [SerializeField]
+        private string m_Name;
+
+        [SerializeField]
+        private HlslSource m_Source;
+
+        public string name 
+        { 
+            get => m_Name; 
+            set => m_Name = value;
+        }
+
+        public HlslSource source 
+        { 
+            get => m_Source; 
+            set => m_Source = value; 
+        }
+        
+        public InputDescriptor[] inArguments { get; set; }
+        public OutputDescriptor[] outArguments { get; set; }
+    }
+}
--- com.unity.shadergraph/Editor/New/Nodes/ListNode.cs
+++ com.unity.shadergraph/Editor/New/Nodes/ListNode.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -8,16 +9,41 @@ using UnityEditor.ShaderGraph.Drawing.Controls;
 
 namespace UnityEditor.ShaderGraph.NodeLibrary
 {
-    sealed class ListNode : ShaderNode, IHasSettings
+    sealed class ListNode : ShaderNode, IHasSettings, IGeneratesBodyCode, IGeneratesFunction
     {
         [SerializeField]
-        public List<InputDescriptor> m_InDescriptors = new List<InputDescriptor>();
+        private List<InputDescriptor> m_InDescriptors = new List<InputDescriptor>();
 
         [SerializeField]
-        public List<InputDescriptor> m_ParameterDescriptors = new List<InputDescriptor>();
+        private List<InputDescriptor> m_ParameterDescriptors = new List<InputDescriptor>();
 
         [SerializeField]
-        public List<OutputDescriptor> m_OutDescriptors = new List<OutputDescriptor>();
+        private List<OutputDescriptor> m_OutDescriptors = new List<OutputDescriptor>();
+
+        [SerializeField]
+        private HlslFunctionDescriptor m_FunctionDescriptor;
+        
+        public HlslFunctionDescriptor functionDescriptor
+        {
+            get
+            {
+                if(m_FunctionDescriptor == null)
+                {
+                    m_FunctionDescriptor = new HlslFunctionDescriptor()
+                    {
+                        name = s_FunctionName,
+                        source = HlslSource.File(s_FunctionSource, true)
+                    };
+                }
+                m_FunctionDescriptor.inArguments = m_InDescriptors.Union(m_ParameterDescriptors).ToArray();
+                m_FunctionDescriptor.outArguments = m_OutDescriptors.ToArray();
+                return m_FunctionDescriptor;
+            }
+            set => m_FunctionDescriptor = value;
+        }
+
+        private static string s_FunctionName =>  "Function name here...";
+        private static string s_FunctionSource => "Hlsl include file path here...";
 
         internal override void Setup(ref NodeDefinitionContext context)
         {
@@ -75,6 +101,95 @@ namespace UnityEditor.ShaderGraph.NodeLibrary
                 descriptor.name += string.Format(" ({0})", duplicateNameCeiling);
         }
 
+        private bool IsValidFunctionDescriptor()
+        {
+            return (!string.IsNullOrEmpty(functionDescriptor.name) &&
+                !string.IsNullOrEmpty(functionDescriptor.source.value) &&
+                functionDescriptor.name != s_FunctionName &&
+                functionDescriptor.source.value != s_FunctionSource);
+        }
+
+        public void GenerateNodeCode(ShaderGenerator visitor, GraphContext graphContext, GenerationMode generationMode)
+        {
+            if(!IsValidFunctionDescriptor())
+                return;
+
+            foreach (var argument in functionDescriptor.outArguments)
+                visitor.AddShaderChunk(argument.valueType.ToString(precision) + " " + GetShaderValue(argument).ToVariableName() + ";", true);
+
+            string call = GetFunctionName(functionDescriptor.name) + "(";
+            bool first = true;
+            foreach (var argument in functionDescriptor.inArguments)
+            {
+                if (!first)
+                    call += ", ";
+                first = false;
+                IShaderValue shaderValue = GetShaderValue(argument);
+                call += shaderValue.ToVariableReference(precision, generationMode);
+            }
+            foreach (var argument in functionDescriptor.outArguments)
+            {
+                if (!first)
+                    call += ", ";
+                first = false;
+                call += GetShaderValue(argument).ToVariableName();
+            }
+            call += ");";
+            visitor.AddShaderChunk(call, true);
+        }
+
+        public void GenerateNodeFunction(FunctionRegistry registry, GraphContext graphContext, GenerationMode generationMode)
+        {
+            if(!IsValidFunctionDescriptor())
+                return;
+
+            registry.ProvideFunction(functionDescriptor.name, builder =>
+            {
+                switch (functionDescriptor.source.type)
+                {
+                    case HlslSourceType.File:
+                        builder.AppendLine($"#include \"{functionDescriptor.source.value}\"");
+                        break;
+                    case HlslSourceType.String:
+                        builder.AppendLine(GetFunctionHeader(functionDescriptor));
+                        using(builder.BlockScope())
+                        {
+                            builder.AppendLines(functionDescriptor.source.value);
+                        }
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            });
+        }
+
+        private string GetFunctionHeader(HlslFunctionDescriptor descriptor)
+        {
+            string header = string.Format("void {0}_{1}(", descriptor.name, precision);
+            var first = true;
+            foreach (var argument in descriptor.inArguments)
+            {
+                if (!first)
+                    header += ", ";
+                first = false;
+                header += string.Format("{0} {1}", argument.valueType.ToString(precision), argument.name);
+            }
+            foreach (var argument in descriptor.outArguments)
+            {
+                if (!first)
+                    header += ", ";
+                first = false;
+                header += string.Format("out {0} {1}", argument.valueType.ToString(precision), argument.name);
+            }
+            header += ")";
+            return header;
+        }
+
+        private string GetFunctionName(string name)
+        {
+            return string.Format("{0}_{1}", name, precision);
+        }
+
         public VisualElement CreateSettingsElement()
         {
             PropertySheet ps = new PropertySheet();
@@ -82,6 +197,7 @@ namespace UnityEditor.ShaderGraph.NodeLibrary
             ps.Add(new InputDescriptorListView(this, m_InDescriptors, ShaderValueDescriptorType.Input));
             ps.Add(new OutputDescriptorListView(this, m_OutDescriptors));
             ps.Add(new InputDescriptorListView(this, m_ParameterDescriptors, ShaderValueDescriptorType.Parameter));
+            ps.Add(new HlslSourceView(this, functionDescriptor));
             return ps;
         }
     }
