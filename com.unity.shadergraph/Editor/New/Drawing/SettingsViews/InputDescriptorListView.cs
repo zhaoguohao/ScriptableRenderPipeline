55df7dfd3a9de4324b2c98a06d45ba2cc6344a8b
Matt Dean <matthewdean3d@gmail.com>
2019-01-26 01:15:29 -0800

    Clean up list views

--- com.unity.shadergraph/Editor/New/Drawing/SettingsViews/InputDescriptorListView.cs
+++ com.unity.shadergraph/Editor/New/Drawing/SettingsViews/InputDescriptorListView.cs
@@ -1,111 +1,31 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using System.Linq;
 using UnityEngine;
-using UnityEditor.UIElements;
-using UnityEngine.UIElements;
-using UnityEditor.Graphing;
 using UnityEditor.Graphing.Util;
-using UnityEditor.ShaderGraph.Drawing.Controls;
 using UnityEditorInternal;
 
 namespace UnityEditor.ShaderGraph.Drawing
 {
-    class InputDescriptorListView : VisualElement
+    class InputDescriptorListView : ShaderValueDescriptorListView
     {
-        // Node data
-        ShaderNode m_Node;
         List<InputDescriptor> m_InputDescriptors;
-        ShaderValueDescriptorType m_DescriptorType;
-
-        // GUI data
-		IMGUIContainer m_Container;
-        ReorderableList m_ReorderableList;
-        int m_SelectedIndex = -1;
 
         public InputDescriptorListView(ShaderNode node, List<InputDescriptor> descriptors, ShaderValueDescriptorType descriptorType)
+            : base (node, descriptorType)
         {
-            // Styling
-            styleSheets.Add(Resources.Load<StyleSheet>("Styles/SettingsViews/InputDescriptorListView"));
-
-            // Set Node data
-            m_Node = node;
             m_InputDescriptors = descriptors;
-            m_DescriptorType = descriptorType;
-
-            // Generate GUI
-            m_Container = new IMGUIContainer(() => OnGUIHandler ()) { name = "ListContainer" };
-            Add(m_Container);
         }
 
-        void OnGUIHandler()
+        internal override ReorderableList CreateList()
         {
-            // If changed
-            using (var changeCheckScope = new EditorGUI.ChangeCheckScope())
-            {
-                // Create Reorderable List
-                if(m_ReorderableList == null)
-                {
-                    var label = string.Format("{0}s", Regex.Replace(m_DescriptorType.ToString(), "(\\B[A-Z])", " $1"));
-                    CreateReorderableList(m_InputDescriptors, label, true, true, true, true);
-                }
-                
-                m_ReorderableList.index = m_SelectedIndex;
-                m_ReorderableList.DoLayoutList();
-
-                // If changed repaint
-                if (changeCheckScope.changed)
-                    m_Node.Dirty(ModificationScope.Node);
-            }
+            return new ReorderableList(m_InputDescriptors, typeof(IShaderValueDescriptor), true, true, true, true);
         }
 
-        private void CreateReorderableList(List<InputDescriptor> list, string label, bool draggable, bool displayHeader, bool displayAddButton, bool displayRemoveButton) 
+        internal override void DrawDescriptorRow(ReorderableList list, int index, Rect rect)
         {
-            m_ReorderableList = new ReorderableList(list, typeof(InputDescriptor), draggable, displayHeader, displayAddButton, displayRemoveButton);
-        
-            m_ReorderableList.drawHeaderCallback = (Rect rect) => 
-            {  
-                var labelRect = new Rect(rect.x, rect.y, rect.width-10, rect.height);
-                EditorGUI.LabelField(labelRect, label);
-            };
-
-            // Draw Element
-            m_ReorderableList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) => 
-            {
-                rect.y += 2;
-                DrawEntry(m_ReorderableList, index, new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight));
-            };
-
-            // Element height
-            m_ReorderableList.elementHeightCallback = (int indexer) => 
-            {
-                return m_ReorderableList.elementHeight;
-            };
-            
-            // Add callback delegates
-            m_ReorderableList.onSelectCallback += SelectEntry;
-            m_ReorderableList.onAddCallback += AddEntry;
-            m_ReorderableList.onRemoveCallback += RemoveEntry;
-            m_ReorderableList.onReorderCallback += ReorderEntries;
-        }
-
-        private void DrawEntry(ReorderableList list, int index, Rect rect)
-        {
-            // Label style
-            GUIStyle labelStyle = new GUIStyle();
-            labelStyle.normal.textColor = Color.white;
-
-            // Get Descriptor data
-            InputDescriptor descriptor = list.list[index] as InputDescriptor;
-            bool hasControl = m_DescriptorType != ShaderValueDescriptorType.Output;
-
-            // Draw element GUI
-            int elementCount = 2;
-            EditorGUI.BeginChangeCheck();
-            var name = EditorGUI.DelayedTextField( new Rect(rect.x, rect.y, rect.width / elementCount, EditorGUIUtility.singleLineHeight), descriptor.name, labelStyle);       
-            var valueType = (ConcreteSlotValueType)EditorGUI.EnumPopup( new Rect(rect.x + rect.width / elementCount, rect.y, rect.width / elementCount, EditorGUIUtility.singleLineHeight), descriptor.valueType);
+            InputDescriptor descriptor = list.list[index] as InputDescriptor;    
+            var valueType = (ConcreteSlotValueType)EditorGUI.EnumPopup( new Rect(rect.x, rect.y, rect.width - labelWidth, EditorGUIUtility.singleLineHeight), descriptor.valueType);
 
             // InputDescriptor inDescriptor = descriptor as InputDescriptor;
             // Type[] validControlTypes = GetAllShaderControlTypeThatSupportValueType(valueType);
@@ -117,50 +37,7 @@ namespace UnityEditor.ShaderGraph.Drawing
             //      GetIndexOfControl(inDescriptor.control.GetType(), valueType),
             //      validControlTypeStrings);
 
-            list.list[index] = new InputDescriptor(descriptor.id, name, valueType, valueType.ToDefaultControl()/*, GetControlFromIndex(controlValue, valueType)*/);
-
-            // Update Descriptors if changed
-            if(EditorGUI.EndChangeCheck())
-                m_Node.ValidateNode();
-        }
-
-        private void SelectEntry(ReorderableList list)
-        {
-            m_SelectedIndex = list.index;
-        }
-
-        private void AddEntry(ReorderableList list)
-        {
-            var defaultType = ConcreteSlotValueType.Vector1;
-            switch(m_DescriptorType)
-            {
-                case ShaderValueDescriptorType.Input:
-                    list.list.Add(new InputDescriptor(-1, "Invalid", defaultType, defaultType.ToDefaultControl()));
-                    m_SelectedIndex = m_InputDescriptors.Count - 1;
-                    break;
-                case ShaderValueDescriptorType.Output:
-                    // Exception
-                    break;
-                case ShaderValueDescriptorType.Parameter:
-                    list.list.Add(new InputDescriptor(-1, "Invalid", defaultType, defaultType.ToDefaultControl()));
-                    m_SelectedIndex = m_InputDescriptors.Count - 1;
-                    break;
-            }
-
-            m_Node.ValidateNode();
-        }
-
-        private void RemoveEntry(ReorderableList list)
-        {
-            list.index = m_SelectedIndex;
-            ReorderableList.defaultBehaviours.DoRemoveButton(list);
-            m_SelectedIndex = list.index;
-            m_Node.ValidateNode();
-        }
-
-        private void ReorderEntries(ReorderableList list)
-        {
-            m_Node.ValidateNode();
+            list.list[index] = new InputDescriptor(descriptor.id, descriptor.name, valueType, valueType.ToDefaultControl()/*, GetControlFromIndex(controlValue, valueType)*/);
         }
 
         // --------------------------------------------------
--- com.unity.shadergraph/Editor/New/Drawing/SettingsViews/OutputDescriptorListView.cs
+++ com.unity.shadergraph/Editor/New/Drawing/SettingsViews/OutputDescriptorListView.cs
@@ -1,137 +1,29 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using System.Linq;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.UIElements;
-using UnityEngine.UIElements;
-using UnityEditor.Graphing;
-using UnityEditor.Graphing.Util;
-using UnityEditor.ShaderGraph.Drawing.Controls;
 using UnityEditorInternal;
 
 namespace UnityEditor.ShaderGraph.Drawing
 {
-    class OutputDescriptorListView : VisualElement
+    class OutputDescriptorListView : ShaderValueDescriptorListView
     {
-        // Node data
-        ShaderNode m_Node;
         List<OutputDescriptor> m_OutputDescriptors;
 
-        // GUI data
-		IMGUIContainer m_Container;
-        ReorderableList m_ReorderableList;
-        int m_SelectedIndex = -1;
-
         public OutputDescriptorListView(ShaderNode node, List<OutputDescriptor> descriptors)
+            : base (node, ShaderValueDescriptorType.Output)
         {
-            // Styling
-            styleSheets.Add(Resources.Load<StyleSheet>("Styles/SettingsViews/OutputDescriptorListView"));
-
-            // Set Node data
-            m_Node = node;
             m_OutputDescriptors = descriptors;
-
-            // Generate GUI
-            m_Container = new IMGUIContainer(() => OnGUIHandler ()) { name = "ListContainer" };
-            Add(m_Container);
         }
 
-        void OnGUIHandler()
+        internal override ReorderableList CreateList()
         {
-            // If changed
-            using (var changeCheckScope = new EditorGUI.ChangeCheckScope())
-            {
-                // Create Reorderable List
-                if(m_ReorderableList == null)
-                    CreateReorderableList(m_OutputDescriptors, "Outputs", true, true, true, true);
-                
-                m_ReorderableList.index = m_SelectedIndex;
-                m_ReorderableList.DoLayoutList();
-
-                // If changed repaint
-                if (changeCheckScope.changed)
-                    m_Node.Dirty(ModificationScope.Node);
-            }
+            return new ReorderableList(m_OutputDescriptors, typeof(IShaderValueDescriptor), true, true, true, true);
         }
 
-        private void CreateReorderableList(List<OutputDescriptor> list, string label, bool draggable, bool displayHeader, bool displayAddButton, bool displayRemoveButton) 
+        internal override void DrawDescriptorRow(ReorderableList list, int index, Rect rect)
         {
-            m_ReorderableList = new ReorderableList(list, typeof(OutputDescriptor), draggable, displayHeader, displayAddButton, displayRemoveButton);
-        
-            m_ReorderableList.drawHeaderCallback = (Rect rect) => 
-            {  
-                var labelRect = new Rect(rect.x, rect.y, rect.width-10, rect.height);
-                EditorGUI.LabelField(labelRect, label);
-            };
-
-            // Draw Element
-            m_ReorderableList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) => 
-            {
-                rect.y += 2;
-                DrawEntry(m_ReorderableList, index, new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight));
-            };
-
-            // Element height
-            m_ReorderableList.elementHeightCallback = (int indexer) => 
-            {
-                return m_ReorderableList.elementHeight;
-            };
-            
-            // Add callback delegates
-            m_ReorderableList.onSelectCallback += SelectEntry;
-            m_ReorderableList.onAddCallback += AddEntry;
-            m_ReorderableList.onRemoveCallback += RemoveEntry;
-            m_ReorderableList.onReorderCallback += ReorderEntries;
-        }
-
-        private void DrawEntry(ReorderableList list, int index, Rect rect)
-        {
-            // Label style
-            GUIStyle labelStyle = new GUIStyle();
-            labelStyle.normal.textColor = Color.white;
-
-            // Get Descriptor data
             OutputDescriptor descriptor = list.list[index] as OutputDescriptor;
-
-            // Draw element GUI
-            int elementCount = 2;
-            EditorGUI.BeginChangeCheck();
-            var name = EditorGUI.DelayedTextField( new Rect(rect.x, rect.y, rect.width / elementCount, EditorGUIUtility.singleLineHeight), descriptor.name, labelStyle);       
-            var valueType = (ConcreteSlotValueType)EditorGUI.EnumPopup( new Rect(rect.x + rect.width / elementCount, rect.y, rect.width / elementCount, EditorGUIUtility.singleLineHeight), descriptor.valueType);
-
-            list.list[index] = new OutputDescriptor(descriptor.id, name, valueType);
-
-            // Update Descriptors if changed
-            if(EditorGUI.EndChangeCheck())
-                m_Node.ValidateNode();
-        }
-
-        private void SelectEntry(ReorderableList list)
-        {
-            m_SelectedIndex = list.index;
-        }
-
-        private void AddEntry(ReorderableList list)
-        {
-            var defaultType = ConcreteSlotValueType.Vector1;
-            list.list.Add(new OutputDescriptor(-1, "Invalid", defaultType));
-            m_SelectedIndex = m_OutputDescriptors.Count - 1;
-            m_Node.ValidateNode();
-        }
-
-        private void RemoveEntry(ReorderableList list)
-        {
-            list.index = m_SelectedIndex;
-            ReorderableList.defaultBehaviours.DoRemoveButton(list);
-            m_SelectedIndex = list.index;
-            m_Node.ValidateNode();
-        }
-
-        private void ReorderEntries(ReorderableList list)
-        {
-            m_Node.ValidateNode();
+            var valueType = (ConcreteSlotValueType)EditorGUI.EnumPopup( new Rect(rect.x, rect.y, rect.width - labelWidth, EditorGUIUtility.singleLineHeight), descriptor.valueType);
+            list.list[index] = new OutputDescriptor(descriptor.id, descriptor.name, valueType);
         }
     }
 }
