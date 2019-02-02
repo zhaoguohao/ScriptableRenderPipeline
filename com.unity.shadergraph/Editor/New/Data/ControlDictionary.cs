248fb8d6db98d04f6cfc82510b8f0651642f037f
Matt Dean <matthewdean3d@gmail.com>
2019-02-01 19:18:49 -0800

    Move control lookups to separate class

---
+++ com.unity.shadergraph/Editor/New/Data/ControlDictionary.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Graphing.Util;
+
+namespace UnityEditor.ShaderGraph
+{
+    struct ControlDictionary
+    {
+        private Dictionary<SlotValueType, Type[]> m_Dictionary;
+        public Dictionary<SlotValueType, Type[]> dictionary
+        {
+            get
+            {
+                if(m_Dictionary == null)
+                    m_Dictionary = Generate();
+                return m_Dictionary;
+            }
+        }
+
+        private List<Type> m_TempList;
+        private List<Type> tempList
+        {
+            get
+            {
+                if(m_TempList == null)
+                    m_TempList = new List<Type>();
+                return m_TempList;
+            }
+        }
+
+        private Dictionary<SlotValueType, Type[]> Generate()
+        {
+            List<IShaderControl> controls = new List<IShaderControl>();
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var type in assembly.GetTypesOrNothing())
+                {
+                    // TODO - Support generic texture types
+                    if (type.IsClass && !type.IsAbstract && typeof(IShaderControl).IsAssignableFrom(type) 
+                        && type != typeof(TextureControl<>) && type != typeof(DefaultControl))
+                        controls.Add(Activator.CreateInstance(type) as IShaderControl);
+                }
+            }
+
+            var output = new Dictionary<SlotValueType, Type[]>();
+            for(int t = 0; t < Enum.GetNames(typeof(SlotValueType)).Length; t++)
+            {
+                tempList.Clear();
+                for(int c = 0; c < controls.Count; c++)
+                {
+                    if(controls[c].validPortTypes.ToList().Contains((SlotValueType)t))
+                        tempList.Add(controls[c].GetType());
+                }
+                output.Add((SlotValueType)t, tempList.ToArray());
+            }
+            return output;
+        }
+
+        public string[] GetControlEntries(SlotValueType valueType)
+        {
+            Type[] types;
+            dictionary.TryGetValue(valueType, out types);
+            return types.Select(s => s.Name).ToArray();
+        }
+
+        public int GetIndexOfControl(SlotValueType valueType, IShaderControl control)
+        {
+            Type[] types;
+            dictionary.TryGetValue(valueType, out types);
+            return Array.IndexOf(types, control.GetType());
+        }
+
+        public IShaderControl GetControlFromIndex(SlotValueType valueType, int index)
+        {
+            Type[] types;
+            dictionary.TryGetValue(valueType, out types);
+            return (IShaderControl)Activator.CreateInstance(types[index]);
+        }
+    }
+}
--- com.unity.shadergraph/Editor/New/Drawing/Views/InputDescriptorListView.cs
+++ com.unity.shadergraph/Editor/New/Drawing/Views/InputDescriptorListView.cs
@@ -9,51 +9,7 @@ namespace UnityEditor.ShaderGraph.Drawing
 {
     class InputDescriptorListView : ShaderValueDescriptorListView
     {
-        private Dictionary<SlotValueType, List<Type>> m_ControlTypeMap;
-
-        public Dictionary<SlotValueType, List<Type>> controlTypeMap
-        {
-            get
-            {
-                if(m_ControlTypeMap == null)
-                    m_ControlTypeMap = GenerateControlTypeMap();
-                return m_ControlTypeMap;
-            }
-        }
-
-        private Dictionary<SlotValueType, List<Type>> GenerateControlTypeMap()
-        {
-            List<IShaderControl> controlInstances = new List<IShaderControl>();
-            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                foreach (var type in assembly.GetTypesOrNothing())
-                {
-                    if (type.IsClass && !type.IsAbstract && typeof(IShaderControl).IsAssignableFrom(type) 
-                        && type != typeof(TextureControl<>) && type != typeof(DefaultControl))
-                        controlInstances.Add(Activator.CreateInstance(type) as IShaderControl);
-                }
-            }
-
-            var typeMap = new Dictionary<SlotValueType, List<Type>>();
-            for(int t = 0; t < Enum.GetNames(typeof(SlotValueType)).Length; t++)
-            {
-                var types = new List<Type>();
-                for(int c = 0; c < controlInstances.Count; c++)
-                {
-                    if(controlInstances[c].validPortTypes.ToList().Contains((SlotValueType)t))
-                        types.Add(controlInstances[c].GetType());
-                }
-                typeMap.Add((SlotValueType)t, types);
-            }
-            return typeMap;
-        }
-
-        private int GetIndexOfControlType(SlotValueType valueType, IShaderControl controlType)
-        {
-            List<Type> types = new List<Type>();
-            controlTypeMap.TryGetValue(valueType, out types);
-            return types.IndexOf(controlType.GetType());
-        }
+        ControlDictionary m_ControlDictionary;
 
         List<InputDescriptor> m_InputDescriptors;
 
@@ -72,13 +28,13 @@ namespace UnityEditor.ShaderGraph.Drawing
         {
             InputDescriptor descriptor = (InputDescriptor)list.list[index]; 
 
-            List<Type> tempTypes = new List<Type>();
             SlotValueType previousValueType = descriptor.valueType;
-            var valueType = (SlotValueType)EditorGUI.EnumPopup( new Rect(rect.x, rect.y, (rect.width - labelWidth) * 0.5f, EditorGUIUtility.singleLineHeight), descriptor.valueType);
-            controlTypeMap.TryGetValue(valueType, out tempTypes);  
-            var controlIndex =  GetIndexOfControlType(valueType, previousValueType != valueType ? valueType.ToDefaultControl() : descriptor.control);
-            var controlType = EditorGUI.Popup(new Rect(rect.x + (rect.width - labelWidth) * 0.5f, rect.y, (rect.width - labelWidth) * 0.5f, EditorGUIUtility.singleLineHeight), controlIndex, tempTypes.Select(s => s.Name).ToArray());
-            list.list[index] = new InputDescriptor(descriptor.id, descriptor.name, valueType, (IShaderControl)Activator.CreateInstance(tempTypes[controlType]));
+            var valueType = (SlotValueType)EditorGUI.EnumPopup( new Rect(rect.x, rect.y, (rect.width - labelWidth) * 0.5f, EditorGUIUtility.singleLineHeight), descriptor.valueType); 
+            
+            var previousControlIndex =  m_ControlDictionary.GetIndexOfControl(valueType, previousValueType != valueType ? valueType.ToDefaultControl() : descriptor.control);
+            var controlIndex = EditorGUI.Popup(new Rect(rect.x + (rect.width - labelWidth) * 0.5f, rect.y, (rect.width - labelWidth) * 0.5f, EditorGUIUtility.singleLineHeight), previousControlIndex, m_ControlDictionary.GetControlEntries(valueType));
+            
+            list.list[index] = new InputDescriptor(descriptor.id, descriptor.name, valueType, m_ControlDictionary.GetControlFromIndex(valueType, controlIndex));
         }
     }
 }
