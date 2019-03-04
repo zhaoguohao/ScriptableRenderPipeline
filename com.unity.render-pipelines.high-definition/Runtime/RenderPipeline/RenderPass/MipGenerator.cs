cddc691a9807584bb757130334f600514aa159fe
FrancescoC-Unity <francescoc@unity3d.com>
2019-03-04 19:13:25 +0100

    Make it an option on the asset

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/RenderPass/MipGenerator.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/RenderPass/MipGenerator.cs
@@ -133,7 +133,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                 m_TempColorTargets[kernelIndex] = RTHandles.Alloc(
                     Vector2.one * 0.5f,
                     filterMode: FilterMode.Bilinear,
-                    colorFormat: HDRenderPipeline.s_ColorBufferFormat,
+                    colorFormat: destination.graphicsFormat,
                     enableRandomWrite: true,
                     useMipMap: false,
                     enableMSAA: false,
